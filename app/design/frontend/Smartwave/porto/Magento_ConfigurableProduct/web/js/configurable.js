ede6dea62493b6785cc679708199e2490405bb7b
Thanh <bhm.rb27@gmail.com>
2023-03-25 02:47:16 +0700

    fix select an option is shown on safari

--- app/design/frontend/Smartwave/porto/Magento_ConfigurableProduct/web/js/configurable.js
+++ app/design/frontend/Smartwave/porto/Magento_ConfigurableProduct/web/js/configurable.js
@@ -447,8 +447,8 @@ define([
             this._clearSelect(element);
             element.options[0] = new Option('', '');
             element.options[0].innerHTML = this.options.spConfig.chooseText;
-            element.options[0].prop("disabled", true); // for safari
-            element.options[0].hide();
+            element.options[0].style.display = 'none';
+            element.options[0].style.disabled = 'true';
             prevConfig = false;
 
             if (element.prevSetting) {
